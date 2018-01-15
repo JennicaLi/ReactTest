ee278aa9d897b14304d581bc7fd3a66e9dac4af3
lijing <yiliyanyu2@163.com>
2018-01-15 15:12:04 +0800

    demo1

--- todoListDemo/src/App.js
+++ todoListDemo/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './Compont/NavBar';
 import ToDos from "./Compont/ToDos";
 
 class App extends Component {
